1ca3cf2c8c81e4a9b53f1b09dd0d735c50bed504
ducsulove2005 <lesu3405@gmail.com>
2026-09-16 13:40:42 +0700

    feat(repository): create ProductRepo extend MongoRepo

--- slot3/product-service/src/main/java/com/fudn/product_service/repository/ProductRepository.java
+++ slot3/product-service/src/main/java/com/fudn/product_service/repository/ProductRepository.java
@@ -2,6 +2,6 @@ package com.fudn.product_service.repository;
 
 import com.fudn.product_service.model.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
-
+//repo
 public interface ProductRepository extends MongoRepository<Product, String> {
 }
