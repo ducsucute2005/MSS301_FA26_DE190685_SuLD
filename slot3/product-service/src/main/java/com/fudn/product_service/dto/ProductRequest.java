4eaec4aff6bc28e00fec20527564505c212ac3ac
ducsulove2005 <lesu3405@gmail.com>
2026-09-16 13:42:04 +0700

    feat(dto): create productRequest and productResponse

--- slot3/product-service/src/main/java/com/fudn/product_service/dto/ProductRequest.java
+++ slot3/product-service/src/main/java/com/fudn/product_service/dto/ProductRequest.java
@@ -1,6 +1,6 @@
 package com.fudn.product_service.dto;
 
 import java.math.BigDecimal;
-
+//dto request
 public record ProductRequest(String name, String description, BigDecimal price) {
 }
--- slot3/product-service/src/main/java/com/fudn/product_service/dto/ProductResponse.java
+++ slot3/product-service/src/main/java/com/fudn/product_service/dto/ProductResponse.java
@@ -1,6 +1,6 @@
 package com.fudn.product_service.dto;
 
 import java.math.BigDecimal;
-
+//dto response
 public record ProductResponse(String id, String name, String description, BigDecimal price) {
 }
