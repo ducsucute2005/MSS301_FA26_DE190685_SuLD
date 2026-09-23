1a9a75e91283af694ca64541fc659b92e24d99f6
ducsulove2005 <lesu3405@gmail.com>
2026-09-23 13:49:01 +0700

    feat(properties): bước 3 config application.properties

---
+++ slot5/inventory-service/src/main/java/com/fudn/inventory_service/repository/OrderRepository.java
@@ -0,0 +1,7 @@
+package com.fudn.inventory_service.repository;
+
+import com.fudn.inventory_service.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
--- slot5/product-service/product-service/src/main/java/com/fudn/product_service/exception/ProductNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.fudn.product_service.exception;
-
-
-public class ProductNotFoundException extends RuntimeException {
-    public ProductNotFoundException(String id) {
-        super("not found product with id: " + id);
-    }
-}
