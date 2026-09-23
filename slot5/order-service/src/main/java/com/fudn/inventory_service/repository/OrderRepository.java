3125d32cb004eb19b3c4717796173a42aec578d2
ducsulove2005 <lesu3405@gmail.com>
2026-09-23 13:54:34 +0700

    feat(repository): bước 6 create repo OrderRepository.java

--- slot5/order-service/src/main/java/com/fudn/inventory_service/repository/OrderRepository.java
+++ slot5/order-service/src/main/java/com/fudn/inventory_service/repository/OrderRepository.java
@@ -2,6 +2,6 @@ package com.fudn.inventory_service.repository;
 
 import com.fudn.inventory_service.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+//repo
 public interface OrderRepository extends JpaRepository<Order, Long> {
 }
