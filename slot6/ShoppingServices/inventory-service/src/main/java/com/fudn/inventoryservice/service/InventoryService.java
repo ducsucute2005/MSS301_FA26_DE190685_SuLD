05b39daff875d2fe503d2dc5e352df90472af9c3
ducsulove2005 <lesu3405@gmail.com>
2026-09-25 16:49:41 +0700

    feat(client): BƯỚC 2 – Tạo FeignClient interface cho Inventory Service

--- slot6/ShoppingServices/inventory-service/src/main/java/com/fudn/inventoryservice/service/InventoryService.java
+++ slot6/ShoppingServices/inventory-service/src/main/java/com/fudn/inventoryservice/service/InventoryService.java
@@ -4,8 +4,6 @@ import com.fudn.inventoryservice.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-
 @Service
 @RequiredArgsConstructor
 public class InventoryService {
