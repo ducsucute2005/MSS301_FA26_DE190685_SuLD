15586ee8b29d77556c34fa916a6b91c227e5c291
ducsulove2005 <lesu3405@gmail.com>
2026-09-25 16:46:47 +0700

    feat(pom): BƯỚC 1 – Thêm dependency OpenFeign vào Order Service

---
+++ slot6/ShoppingServices/inventory-service/src/main/java/com/fudn/inventoryservice/repository/InventoryRepository.java
@@ -0,0 +1,10 @@
+package com.fudn.inventoryservice.repository;
+
+import com.fudn.inventoryservice.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+
+
+    boolean existsBySkuCodeAndQuantityIsGreaterThanEqual(String skuCode, int quantity);
+}
