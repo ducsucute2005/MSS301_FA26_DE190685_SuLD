6498236df0be23acfc9bbc1a759a919b1929dfc2
ducsulove2005 <lesu3405@gmail.com>
2026-09-25 15:30:13 +0700

    feat(controller): Bước 8 – Tạo Controller InventoryController.java

--- slot5/inventory-service/src/main/java/com/fudn/inventoryservice/controller/InventoryController.java
+++ slot5/inventory-service/src/main/java/com/fudn/inventoryservice/controller/InventoryController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+
 @RestController
 @RequestMapping("/api/inventory")
 @RequiredArgsConstructor
