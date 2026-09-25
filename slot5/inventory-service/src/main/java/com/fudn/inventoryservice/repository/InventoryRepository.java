2958c8714512203eb8e1f1d0b8899e8d41d823bb
ducsulove2005 <lesu3405@gmail.com>
2026-09-25 15:29:04 +0700

    feat(repository): Bước 6 – Tạo Repository

--- slot5/inventory-service/src/main/java/com/fudn/inventoryservice/repository/InventoryRepository.java
+++ slot5/inventory-service/src/main/java/com/fudn/inventoryservice/repository/InventoryRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
 
-    // Spring Data JPA tự sinh query từ tên method:
-    // SELECT COUNT(*) > 0 FROM t_inventory WHERE sku_code = ? AND quantity >= ?
+
     boolean existsBySkuCodeAndQuantityIsGreaterThanEqual(String skuCode, int quantity);
 }
